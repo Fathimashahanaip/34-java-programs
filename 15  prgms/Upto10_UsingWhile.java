972c8591c3790d5495293f7f5eb15581dca2416d
fathima shahana i.p <79891497+Fathimashahanaip@users.noreply.github.com>
2021-05-26 20:07:05 +0530

    Add files via upload
    hf

---
+++ 15  prgms/Upto10_UsingWhile.java
@@ -0,0 +1,13 @@
+class Upto10_UsingWhile 
+{
+	public static void main(String args[])
+	{
+		int i=1;
+		System.out.println("Output is : ");
+		while(i<=10)	
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
