6f38ba2e04eb5ff44fcef28748ec84253626099a
fathima shahana i.p <79891497+Fathimashahanaip@users.noreply.github.com>
2021-06-23 18:05:22 +0530

    Add files via upload
    fdgfd

---
+++ 15  prgms/Array_Elemt_For.java
@@ -0,0 +1,8 @@
+class Array_Elemt_For {
+    public static void main(String[] args) {
+        String[] arr={"jazz","popz","christy"};
+        for(int i=0;i < arr.length;i++){
+            System.out.println("Element "+ (i+1) +" : "+arr[i]);
+        }
+    }
+}
